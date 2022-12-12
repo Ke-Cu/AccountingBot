3dfa68c3c6efb3b517baed9dc30f9a8f54f2e3cc
KamenRiderKuuga <524472212@qq.com>
2022-12-12 14:01:29 +0800

    feat: adjust the method for getting timestamp from time string

--- AccountingBot/TimeHelper.cs
+++ AccountingBot/TimeHelper.cs
@@ -15,13 +15,22 @@
         /// </summary>
         /// <param name="timeText">时间字符串，如yyyy-MM-dd HH:mm:ss</param>
         /// <param name="offset">时间偏移量</param>
+        /// <param name="appendOffsetIfZero">当转换的时间字符串时间偏移量为0时，为其添加当前时间偏移量信息</param>
         /// <returns></returns>
         /// <remarks>支持带时区信息的字符串，例如一个东6区时间：2000-01-01 00:00:00 +06:00</remarks>
-        public static long GetLocalTimeFromTimeString(string timeText, int offset = 8)
+        public static long GetLocalTimeFromTimeString(string timeText, int offset = 8, bool appendOffsetIfZero = true)
         {
-            _ = DateTimeOffset.TryParse(timeText, out var time);
-            time += time.Offset + (TimeSpan.FromHours(offset) - time.Offset);
-            return time.AddHours(-offset).ToUnixTimeMilliseconds();
+            var success = DateTimeOffset.TryParse(timeText, null, System.Globalization.DateTimeStyles.AssumeUniversal, out var time);
+
+            if (success)
+            {
+                if (time.Offset.Hours == 0 && appendOffsetIfZero)
+                {
+                    time = time.AddHours(-offset);
+                }
+            }
+
+            return time.ToUnixTimeMilliseconds();
         }
     }
 }
